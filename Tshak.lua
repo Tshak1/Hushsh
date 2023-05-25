9d8cfb3e44442e4c769ffbfc2997b5081e0030d0
Tshak1 <134204773+Tshak1@users.noreply.github.com>
2023-05-25 12:16:03 +0300

    Update Tshak.lua

--- Tshak.lua
+++ Tshak.lua
@@ -5756,16 +5756,16 @@ local R = Redis:scard(Tshak.."Tshak:List:Rd:Sudo")
 local reply_markup = LuaTele.replyMarkup{type = 'inline',data = {{{text = '‹ اسألني في الدين›', data = msg.sender_id.user_id..'/Song'},},{{text = '‹ اسالني في كره قدم ›', data = msg.sender_id.user_id..'/voice'},{text = '‹ اسالني عن العراق ›', data = msg.sender_id.user_id..'/Mp'},},{{text = '‹  اسالني في التاريخ ›', data = msg.sender_id.user_id..'/Memz'},{text = '‹ اسالني في الحيوانات ›', data = msg.sender_id.user_id..'/Remix'},},{{text = '‹  اسالني في الالعاب  ›', data = msg.sender_id.user_id..'/Anime'},{text = '‹  اسالني عن الدول ›', data = msg.sender_id.user_id..'/Photos'},},{{text = '‹  اسالني في الانمي ›', data = msg.sender_id.user_id..'/Series'},{text = '‹ اسالني في الافلام ›', data = msg.sender_id.user_id..'/Movies'},},{{text = '‹ اسالني عن فرقه BtS ›', data = msg.sender_id.user_id..'/animation'},},{{text = '- سورس تشاك .', url = 't.me/Tshaak'},},}}
 return LuaTele.sendText(msg_chat_id, msg_id, "↯︙يمكنك عزيزي من هنا اختيار ماهي الاسئله التي تريدها .", 'md', false, false, false, false, reply_markup) end
 -- Lar --
-elseif Text and Text:match('(%d+)/Phot') then
-local UserId = Text:match('(%d+)/Phot')
-if tonumber(IdUser) == tonumber(UserId) then
-if not Redis:get(Tshak.."Tshak:Status:distraction9"..data.chat_id) then return LuaTele.answerCallbackQuery(data.id,"↯︙عذراً امر صوره معطل",true) end 
-Abs = math.random(4,1171); 
-local Text ='↯︙تم اختيار الصوره لك'
-keyboard = {} 
-keyboard.inline_keyboard = {{{text = '‹ مره اخرى ›', callback_data = IdUser..'/'.. 'Phot'}},{{text='- سورس تشاك .',url="t.me/Tshaak"}}}
-local msg_id = Msg_id/2097152/0.5
- https.request("https://api.telegram.org/bot"..Token..'/sendphoto?chat_id=' .. ChatId .. '&photo=https://t.me/XFFX5/'..Abs..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_prsjeview=true&reply_markup="..JSON.encode(keyboard)) end
+if text == "افتار رسم" or text == "افتارات رسم" or text == "رسم" then 
+Abs = math.random(3,102);
+local Text =''
+local MsgId = msg.id/2097152/0.5
+local MSGID = string.gsub(MsgId,'.0','')
+keyboard = {}  
+keyboard.inline_keyboard = {{{text = '𝐇𝐒𝐎‌ 𝑺𝑶𝑼𝑹𝑪𝑬 ™®',url="t.me/TeamHso"}}} 
+local msg_id = msg.id/2097152/0.5 
+https.request("https://api.telegram.org/bot"..Token..'/sendphoto?chat_id=' .. msg.chat_id .. '&photo=https://t.me/sourseselv/'..Abs..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
+end
 if text == 'رابط الحذف' or text == 'روابط الحذف' or text == 'بوت الحذف' or text == 'بوت حذف' then
 Text =[[
 ● Hello pro buttons at the bottom to delete social media accounts .
