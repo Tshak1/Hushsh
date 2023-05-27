a3f68f2e19f444cb9ec11c907993dda3d47c9dce
Tshak1 <134204773+Tshak1@users.noreply.github.com>
2023-05-27 10:23:58 +0300

    Update Tshak.lua

--- Tshak.lua
+++ Tshak.lua
@@ -5662,7 +5662,7 @@ if text == "فلم" or text == "فيلم" then
 if not Redis:get(Tshak.."Tshak:Status:distraction4"..msg_chat_id) then return LuaTele.sendText(msg_chat_id,msg_id,"↯︙عذراً امر فلم معطل","md",true) end 
 Abs = math.random(4,125); 
 local Text ='↯︙تم اختيار الفلم لك'
-keyboardd = {} 
+keyboard = {} 
 keyboard.inline_keyboard = {{{text = '‹ مره اخرى ›', callback_data = msg.sender_id.user_id..'/Movies'}},{{text='- سورس تشاك .',url="t.me/sourcetshak"}
 }
 }
@@ -5673,7 +5673,7 @@ if text == "انمي" or text == "انميي" then
 if not Redis:get(Tshak.."Tshak:Status:distraction7"..msg_chat_id) then return LuaTele.sendText(msg_chat_id,msg_id,"↯︙عذراً امر انمي معطل","md",true) end 
 Abs = math.random(4,1002); 
 local Text ='↯︙تم اختيار الانمي لك'
-keyboardd = {} 
+keyboard = {} 
 keyboard.inline_keyboard = {{{text = '‹ مره اخرى ›', callback_data = msg.sender_id.user_id..'/Anime'}},{{text='- سورس تشاك .',url="t.me/sourcetshak"}
 }
 }
